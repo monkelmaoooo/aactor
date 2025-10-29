5904aeb1ef9c293db1d5f4f833b8ffd2a4a528fd
monkelmaoooo <157399165+monkelmaoooo@users.noreply.github.com>
2025-10-29 19:22:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-<script>
-  const text = "Monke 🎃";
-  let i = 0;
-  let forward = true;
+const text = "Monke 🎃";
+let i = 0;
+let forward = true;
+let delay = 200;
+let pause = false;
 
-  setInterval(() => {
+function animateTitle() {
+  if (!pause) {
     if (forward) {
       i++;
-      if (i === text.length) forward = false;
+      if (i === text.length) {
+        forward = false;
+        pause = true;
+        setTimeout(() => pause = false, 1000);
+      }
     } else {
       i--;
-      if (i === 0) forward = true;
+      if (i === 0) {
+        forward = true;
+        pause = true;
+        setTimeout(() => pause = false, 500);
+      }
     }
     document.title = text.substring(0, i);
-  }, 200);
-</script>
+  }
+}
+
+setInterval(animateTitle, delay);
